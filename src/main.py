9998916b932f8b5cfa16a70a14b87c20d48eee72
Cezar R <cez.rata1@gmail.com>
2022-04-18 18:56:03 -0700

    version 1.0

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from bot import Bot
+
+def main():
+	bot = Bot()
+	bot.run()
+
+
+if __name__ == '__main__':
+	main()
---
+++ src/words.py
@@ -0,0 +1,10 @@
+def read_file(filename):
+	file = open(filename)
+	content = []
+	for word in file.readlines():
+		content.append(word.strip())
+	return content
+
+
+WORDBANK = read_file("wordbank.txt")
+PREV_ANSWERS = read_file("prev_answers.txt")
