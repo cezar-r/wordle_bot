d065a572ae1957c432827c970838d8e0d61f8ebd
cezar-r <59450965+cezar-r@users.noreply.github.com>
2022-05-03 18:44:59 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,28 +7,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-
-'''
-Psuedo
-
-class Bot:
-
-	interacts w// Wordle website
-		selenium
-	stores data
-		data.json
-		write()
-		read()
-	checks for new wordle
-	plays wordle
-		use info from Wordle website
-		use algo to solve
-			for every possible word after obtaining guess results
-				find all possible evaluation combinations
-				find entropy of word using sum(p*log2(1/p))
-			guess = max(entropy)
-		send guess to Wordle website
-	display
-		read data and display
-'''
